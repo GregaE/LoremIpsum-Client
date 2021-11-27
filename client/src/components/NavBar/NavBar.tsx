09b0c678e2b23924a86cf4b224707c9ce22e9344
Tomazzo Antonioli <tomazzo10@hotmail.com>
2021-11-27 13:13:38 +0100

    one more

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -4,22 +4,16 @@ import {  Link } from "react-router-dom";
 export default function NavBar() {
 
   return (
-    <div className="bg-primary text-light w-1/6 h-screen flex flex-col items-center gap-6">
-      <div className="w-4/6 h-1/6 m-5">
-        <img className="rounded-full" src="https://www.lovemysalad.com/sites/default/files/styles/image_530x397/public/banaan-large.jpg?itok=dZxH6D_w" alt="logo"/>
-      </div>
-      <ul className="flex flex-col flex-wrap justify-around items-center h-3/6 w-full">
-        <li className="nav-link">
+    <div className="bg-primary-bg">
+      <ul>
+        <li>
           <Link to="/">Home</Link>
         </li>
-        <li className="nav-link">
+        <li>
           <Link to="/CVBuilder">CVBuilder</Link>
         </li>
-        <li className="nav-link">
+        <li>
           <Link to="/MyCVs">MyCVs</Link>
-        </li>
-        <li className="nav-link">
-          <Link to="/Profile">Profile</Link>
         </li>
       </ul>
     </div>
