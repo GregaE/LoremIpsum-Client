1cd270bb7709ccc63449e1fcc31389b418eeb5ea
Tomazzo Antonioli <tomazzo10@hotmail.com>
2021-11-27 14:45:41 +0100

    updating: first style step done

--- client/src/components/Home/Calendar/Calendar.tsx
+++ client/src/components/Home/Calendar/Calendar.tsx
@@ -5,7 +5,7 @@ export default function Calendar() {
 
   return (
     <div id="" className="w-full h-1/2 flex flex-row justify-center content-center">
-      <div className="w-5/6 h-4/5 bg-primary my-auto flex flex-row rounded-lg justify-center items-center">
+      <div className="w-96 h-96 bg-primary my-auto flex flex-row rounded-lg justify-center items-center">
         <p>Calendar</p>
       </div>
     </div>
--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -3,21 +3,28 @@ import React from 'react';
 export default function Profile() {
 
   return (
-    <div>
-      <h2>Your personal data</h2>
-      <div className="flex p-4 ml-5">
+    <div className="p-2">
+      <h2 className="underline text-3xl">Your personal data</h2>
+      <div className="flex flex-row justify-center items-center p-4 ml-5">
         <div className="w-1/6 h-1/6 m-5">
         <img src="https://i.graphicmama.com/uploads/2019/3/5c948ff89ffbc-Cute%20Banana%20Cartoon%20Vector%20Character.png" alt="profile-img"/>
         </div>
-        <div className="w-2/3 h-1/6 flex flex-col w-auto h-1/6 m-5 justify-center bg-primary rounded-lg">
-          <p className="p-4">Hello, Banana Joe!</p>
-          <p>Name: Banana Joe John</p>
-          <p>E-mail: joe_banana@bananas.com</p>
-          <p>Password: change your password</p>
+        <div className="w-2/3 h-1/6 flex flex-col w-auto p-4 h-1/6 m-5 justify-center bg-primary rounded-lg">
+          <p className="p-4 underline text-2xl">Hello, Banana Joe!</p>
+          <div className="p-8">
+            <p>Name: Banana Joe John</p>
+            <p>E-mail: joe_banana@bananas.com</p>
+            <p>Password: change your password</p>
+          </div>
         </div>
       </div>
-      <div>
-        <h2>Your categories</h2>
+      <div className="p-2">
+        <h2 className="underline text-3xl">Your categories</h2>
+        <div className="flex flex-col content-around p-4">
+          <p className="w-full bg-light m-2 p-1 rounded-lg">SKILLS</p>
+          <p className="w-full bg-light m-2 p-1 rounded-lg">EDUCATION</p>
+          <p className="w-full bg-light m-2 p-1 rounded-lg">WORK EXPERIENCE</p>
+        </div>
       </div>
     </div>
   );
