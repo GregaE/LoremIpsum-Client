8e7ddc2b308dc754b3e4c6350e06b2d2080cce91
Tomazzo Antonioli <tomazzo10@hotmail.com>
2021-11-29 11:02:25 +0100

    updating pdf renderer

--- client/src/components/CVBuilder/Builder/BuilderSettings/BuilderSettings.tsx
+++ client/src/components/CVBuilder/Builder/BuilderSettings/BuilderSettings.tsx
@@ -7,7 +7,7 @@ export default function BuilderSettings() {
   return (
     <div onClick={() => <PDFRender/>} >
       <div className="flex justify-center bg-primary text-light rounded-lg p-1 m-5">
-        <Button name="Check PDF"/>
+        <Button name="Download"/>
       </div>
     </div>
   );
