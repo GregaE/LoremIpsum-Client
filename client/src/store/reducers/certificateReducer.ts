5de23cbed76b66801e4d31c46fd8c452b482b683
proyev <prots.yevhen@gmail.com>
2021-11-30 04:21:25 +0100

    Create cert reducer

---
+++ client/src/store/reducers/certificateReducer.ts
@@ -0,0 +1,63 @@
+import { CertificateState, CertificateAction } from "../interfaces/certificates";
+
+const initState: CertificateState = {
+  certificates: [],
+  loading: false,
+  error: null
+}
+
+const certificateReducer = (state = initState, {type, payload, id}: CertificateAction) => {
+
+  switch (type) {
+    case 'LOADING':
+      return {
+        ...state,
+        laoding: true,
+        error: null
+      }
+    case 'FAILED':
+      return {
+        ...state,
+        loading: false,
+        error: payload
+      }
+    case 'ALL_CERTIFICATES':
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        certificates: payload
+      }
+    case 'GET_CERTIFICATE':
+      state = {
+        ...state,
+        loading: false,
+        error: null
+      }
+      return state.certificates.filter(certificate => certificate.id === id);
+    case 'UPDATE_CERTIFICATE':
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        certificates: state.certificates.map(certificate => {
+          if (certificate.id === id) return {
+            ...certificate,
+            ...payload
+          }
+          return certificate;
+        })
+      }
+    case 'DELETE_CERTIFICATE':
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        certificates: state.certificates.filter(certificate => certificate.id !== id)
+      }
+    default:
+      return state;
+  }
+}
+
+export default certificateReducer;
