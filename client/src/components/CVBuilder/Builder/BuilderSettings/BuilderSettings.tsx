d1cc37cce571194baecd0123590379a5d035891f
Eugen Nikolajev <zenicek@gmail.com>
2021-11-30 19:25:47 +0100

    merge with Eugen

--- client/src/components/CVBuilder/Builder/BuilderSettings/BuilderSettings.tsx
+++ client/src/components/CVBuilder/Builder/BuilderSettings/BuilderSettings.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Button from '../../../Forms/Elements/Buttons/Button';
-import PDFRender from '../../Modal/ItemEditor/PDF-Render/PDF-Render';
+import { toggleModal } from '../../../../redux/AppState/actionCreators/toggleModal';
+import { useDispatch } from 'react-redux';
 
 export default function BuilderSettings() {
+  const dispatch = useDispatch();
+
+  function handleClick () {
+    // dispatch(toggleModal(true, 'pdf'));
+  }
 
   return (
     <div>
-      <div className="flex justify-center bg-primary text-light rounded-lg p-1 m-5">
+      <div className="flex justify-center bg-primary text-light rounded-lg p-1 m-5" onClick={() => handleClick()}>
         <Button name="Download"/>
       </div>
     </div>
