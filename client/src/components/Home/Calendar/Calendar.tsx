b844d893934d29e848a365ddb7154846f3dd684c
Tomazzo Antonioli <tomazzo10@hotmail.com>
2021-11-30 19:28:20 +0100

    calendar added

--- client/src/components/Home/Calendar/Calendar.tsx
+++ client/src/components/Home/Calendar/Calendar.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
-
-export default function Calendar() {
-
+export default function CalendarComp() {
+  const [value, onChange] = useState(new Date());
   /*
   Nothing yet but we will fetch data from user
   */
 
   return (
-    <div id="" className="w-full h-1/2 p-4 flex flex-row justify-center content-center">
-      <div className="w-96 h-96 bg-light my-auto flex flex-row rounded-lg justify-center items-center">
-        <p>Calendar</p>
-      </div>
+    <div id="" className="w-full h-1/2 p-4 flex justify-center content-center">
+      <div className="w-96 h-96 my-auto justify-center">
+      <Calendar
+        onChange={onChange}
+        value={value}
+      />
+    </div>
     </div>
   );
 }
