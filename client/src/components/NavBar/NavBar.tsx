d7469fb889984a1386e56b1d6e2aa9d58b043651
Eugen Nikolajev <zenicek@gmail.com>
2021-11-30 14:20:47 +0100

    merge

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -23,7 +23,12 @@ export default function NavBar() {
       </div>
       <ul className="flex flex-col flex-wrap justify-around items-center h-3/6 w-full">
         <li className="nav-link" onClick={() => navigateAround('Home')}>
-          <Link to="/">Home</Link>
+          <Link to="/">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="white">
+                <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+            </svg>
+            <span>Home</span>
+          </Link>
         </li>
         <li className="nav-link" onClick={() => navigateAround('CVBuilder')}>
           <Link to="/CVBuilder">CVBuilder</Link>
