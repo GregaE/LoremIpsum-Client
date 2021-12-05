cfdc382a9f6bf514f964245428afe9c89d7f8dd9
proyev <prots.yevhen@gmail.com>
2021-12-05 21:01:06 +0100

    Changes to fetching stuff

--- client/src/utils/ApiService.ts
+++ client/src/utils/ApiService.ts
@@ -8,23 +8,28 @@ async function fetchApi<T>(slug: string, options?: Options): Promise<T> {
   return res.json();
 }
 
-export function getUser(id: string): Promise<User> {
-  return fetchApi<User>(id);
+export async function getUser(id: string): Promise<User> {
+  return await fetchApi<User>(id);
 }
 
-export async function LoginService (user: loginInterface)  {
-  try {
-    const res = await fetch(`${BASE_URL}/login`, {
-      method: 'POST',
-      credentials: 'include',
-      mode: 'cors',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(user),
-    });
-    return await res.json();
-  } catch (err) {
-    return console.error(err);
-  }
+export function LoginService (user: loginInterface): any  {
+   fetch(`${BASE_URL}/login`, {
+    method: 'POST',
+    credentials: 'include',
+    mode: 'cors',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(user),
+  }).then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+    throw new Error(
+      'Error ' + res.status + ': ' + res.statusText
+    );
+  },
+  error => {
+    throw new Error(error.message);
+  });
 }
 
 export async function LogoutService ()  {
@@ -35,23 +40,28 @@ export async function LogoutService ()  {
       mode: 'cors',
       headers: { 'Content-Type': 'application/json' },
     });
-    return await res.json();
+    return res.json();
   } catch (err) {
     return console.error(err);
   }
 }
 
-export async function RegisterService (user: loginInterface) {
-  try {
-    const res = await fetch(`${BASE_URL}/register`, {
-      method: 'POST',
-      credentials: 'include',
-      mode: 'cors',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(user),
-    });
-    return await res.json();
-  } catch (err) {
-    return console.error(err);
-  }
+export function RegisterService (user: loginInterface): any {
+  fetch(`${BASE_URL}/register`, {
+    method: 'POST',
+    credentials: 'include',
+    mode: 'cors',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(user),
+  }).then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+    throw new Error(
+      'Error ' + res.status + ': ' + res.statusText
+    );
+  },
+  error => {
+    throw new Error(error.message);
+  });
 }
