d63901de1a4c6991cc6724abaf4b461afd8acd61
Tomazzo Antonioli <tomazzo10@hotmail.com>
2021-12-07 16:17:26 +0100

    delete each item ok

--- client/src/components/CVBuilder/Builder/Categories/Category/CategoryItem/CategoryItem.tsx
+++ client/src/components/CVBuilder/Builder/Categories/Category/CategoryItem/CategoryItem.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { motion } from 'framer-motion';
-import { DotsVerticalIcon } from '@heroicons/react/solid';
+import { TrashIcon, PencilIcon } from '@heroicons/react/solid';
 import {
   Categories,
   EnumCategories,
@@ -11,6 +11,7 @@ import {
   unselectItem,
 } from '../../../../../../store/actions/pdfActions';
 import { useTypedSelector } from '../../../../../../utils/useTypeSelector';
+import { toggleModal } from '../../../../../../store/actions/toggleModal';
 
 export default function CategoryItem({
   item,
@@ -21,6 +22,13 @@ export default function CategoryItem({
 }) {
   const dispatch = useDispatch();
   const pdfItems = useTypedSelector(state => state.pdf);
+  const {
+    languages: { languages },
+    certificates: { certificates },
+    skills: { skills },
+    education: { education },
+    experience: { experience },
+  } = useTypedSelector(state => state);
 
   const [selected, toggleSelection] = useState(
     pdfItems &&
@@ -28,6 +36,69 @@ export default function CategoryItem({
         .find(pdfI => pdfI.name === categoryName)!
         .pdf.some(pd => pd.id === item.id)
   );
+
+  // function to handle edit option
+  const handleEdit = () => {
+    dispatch(toggleModal(true, categoryName));
+    switch (categoryName) {
+      case 'Certificates':
+        const res = certificates.find(
+          certificate => certificate.id === item.id
+        );
+        console.log(res);
+        dispatch({type: 'FETCH_DATA',  endpoint:'/certificates', method: 'GET', id: item.id, dispatch: 'UPDATE_CERTIFICATES'});
+        break;
+
+    //   case 'Education':
+    //     dispatch({type: 'FETCH_DATA',  endpoint:'/education', method: 'PUT',id: item.id, dispatch: 'UPDATE_EDUCATION'})
+    //     break;
+
+    //   case 'Languages':
+    //     dispatch({type: 'FETCH_DATA',  endpoint:'/languages', method: 'PUT',id: item.id, dispatch: 'UPDATE_LANGUAGES'})
+    //     break;
+
+    //   case 'Skills':
+    //     dispatch({type: 'FETCH_DATA',  endpoint:'/skills', method: 'PUT',id: item.id, dispatch: 'UPDATE_SKILLS'})
+    //     break;
+
+    //   case 'Work Experience':
+    //     dispatch({type: 'FETCH_DATA',  endpoint:'/workExperience', method: 'PUT',id: item.id, dispatch: 'UPDATE_EXPERIENCE'})
+    //     break;
+
+    //   default:
+    //     break;
+    }
+  }
+
+  // function to handle delete option
+  const handleDelete = () => {
+    if (window.confirm('Are you sure you want to delete this?')) {
+      switch (categoryName) {
+        case 'Certificates':
+          dispatch({type: 'FETCH_DATA',  endpoint:'/certificates', method: 'DELETE',id: item.id, dispatch: 'DELETE_CERTIFICATE'})
+          break;
+
+        case 'Education':
+          dispatch({type: 'FETCH_DATA',  endpoint:'/education', method: 'DELETE',id: item.id, dispatch: 'DELETE_EDUCATION'})
+          break;
+
+        case 'Languages':
+          dispatch({type: 'FETCH_DATA',  endpoint:'/languages', method: 'DELETE',id: item.id, dispatch: 'DELETE_LANGUAGE'})
+          break;
+
+        case 'Skills':
+          dispatch({type: 'FETCH_DATA',  endpoint:'/skills', method: 'DELETE',id: item.id, dispatch: 'DELETE_SKILL'})
+          break;
+
+        case 'Work Experience':
+          dispatch({type: 'FETCH_DATA',  endpoint:'/workExperience', method: 'DELETE',id: item.id, dispatch: 'DELETE_EXPERIENCE'})
+          break;
+
+        default:
+          break;
+      }
+    }
+  }
 
   //Manipulate here
   const itemName = () => {
@@ -45,6 +116,7 @@ export default function CategoryItem({
   };
 
   return (
+    <div>
     <motion.div
       key="5" /// needs to pass actual key
       initial={{ opacity: 0, height: '0px' }}
@@ -52,8 +124,8 @@ export default function CategoryItem({
       transition={{ type: 'tween' }}
       exit={{ opacity: 0, height: '0px' }}
       className="item-container"
-    >
-      <div className="group flex flex-row w-full ">
+      >
+      <div className="group flex items-center flex-row w-full ">
         <div
           onClick={() => handleSelection()}
           className="m-1 flex p-1 w-full cursor-pointer"
@@ -67,8 +139,19 @@ export default function CategoryItem({
             {itemName()}
           </span>
         </div>
-        <DotsVerticalIcon className="h-5 w-5 invisible group-hover:visible justify-self-end self-center cursor-pointer"></DotsVerticalIcon>
+        {/* Delete and edit buttons */}
+          <div className="flex invisible group-hover:visible">
+          <button onClick={() => handleEdit()} 
+            className="h-5 w-5 justify-self-end self-center cursor-pointer mr-2">
+            <PencilIcon className="opacity-10 hover:opacity-100"></PencilIcon>
+          </button>
+          <button onClick={() => handleDelete()}
+            className="h-7 w-7 p-1 justify-self-end self-center cursor-pointer mr-2 rounded-full">
+            <TrashIcon className="text-danger opacity-10 hover:opacity-100"></TrashIcon>
+          </button>
+        </div>
       </div>
     </motion.div>
+  </div>
   );
 }
